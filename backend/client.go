cb66a73eb3ba8a4775ecaadf3cea62429933f374
ian.liu <ian.liu@ucloud.cn>
2021-01-05 16:27:33 +0800

    add backend interface

--- backend/client.go
+++ backend/client.go
@@ -1,5 +1,7 @@
 package backend
 
+import "github.com/jinzhu/gorm"
+
 type Client interface {
 	Close() error
 	NewClient() interface{}
@@ -22,3 +24,22 @@ func (b BaseClient) NewClient() interface{} {
 func (b BaseClient) ShowType() string {
 	return b.ClientType
 }
+
+type Backend interface {
+	NewBackend() Backend
+	Close() error
+}
+
+type DataBase struct {
+	Self   *gorm.DB
+	Docker *gorm.DB
+}
+
+func (d *DataBase) NewBackend() Backend {
+	panic("implement me")
+}
+
+func (d *DataBase) Close() error {
+	panic("implement me")
+}
+
