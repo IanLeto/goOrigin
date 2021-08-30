49d9139e4e74cb7ac78e7a970561fe2e5866c9a2
ian.liu <ian.liu@ucloud.cn>
2021-08-30 16:27:17 +0800

    x

--- model/shoadowPriest.go
+++ model/shoadowPriest.go
@@ -3,14 +3,14 @@ package model
 import "time"
 
 type IanUI struct {
-	Id         string
-	Weight     int
-	CodingLine int
-	Task       []DailyTask
-	GetUp      time.Time
-	Sleep      time.Time
-	Cost       int
-	Deposit    int
+	Id         string      `json:"id"`
+	Weight     int         `json:"weight"`
+	CodingLine int         `json:"coding_line"`
+	Task       []DailyTask `json:"task"`
+	GetUp      time.Time   `json:"get_up"`
+	Sleep      time.Time   `json:"sleep"`
+	Cost       int         `json:"cost"`
+	Deposit    int         `json:"deposit"`
 	DailyTask
 	HavingFun
 }
--- storage/mongo.go
+++ storage/mongo.go
@@ -14,7 +14,8 @@ type MongoConn struct {
 
 func NewMongoConn() *MongoConn {
 	mongConf := config.Conf.Backend.MongoBackendConfig
-	url := fmt.Sprintf("mongodb://%s:%s@%s:%s/%s", mongConf.User, mongConf.Password, mongConf.Address, mongConf.Port, mongConf.DB)
+	//url := fmt.Sprintf("mongodb://%s:%s@%s:%s/%s", mongConf.User, mongConf.Password, mongConf.Address, mongConf.Port, mongConf.DB)
+	url := fmt.Sprintf("mongodb://%s:%s/%s", mongConf.Address, mongConf.Port, mongConf.DB)
 	session, err := mgo.Dial(url)
 	if err != nil {
 		logrus.Errorf("init mongo db fail: %s", err)
