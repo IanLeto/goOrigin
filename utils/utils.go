7d5991cc2d3dcb3ddbb5b7999089dc39872af7a2
ian.liu <ian.liu@ucloud.cn>
2021-09-04 20:34:35 +0800

    x

---
+++ utils/utils.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+func EnsureJson(c *gin.Context, v interface{}) error {
+	if err := c.ShouldBindJSON(v); err != nil {
+		logrus.Errorf("数据格式不对 %s", err)
+		return err
+	}
+	return nil
+}
