87b80210eae69704913f6e72efef87de52d20a98
ian.liu <ian.liu@ucloud.cn>
2021-10-04 23:24:53 +0800

    rmIan

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -13,13 +15,13 @@ var (
 	Root = filepath.Join(filepath.Dir(b), "../..")
 )
 
-//func EnsureJson(c *gin.Context, v interface{}) error {
-//	if err := c.ShouldBindJSON(v); err != nil {
-//		logrus.Errorf("数据格式不对 %s", err)
-//		return err
-//	}
-//	return nil
-//}
+func EnsureJson(c *gin.Context, v interface{}) error {
+	if err := c.ShouldBindJSON(v); err != nil {
+		logrus.Errorf("数据格式不对 %s", err)
+		return err
+	}
+	return nil
+}
 
 func IncludeString(s []string, e string) bool {
 	for _, a := range s {
