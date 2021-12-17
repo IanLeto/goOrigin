d9f2bafd935963396f12e99d73843d177fce42c3
ian.liu <ian.liu@ucloud.cn>
2021-12-17 14:47:43 +0800

    x

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
+	"fmt"
 	cmd "goOrigin/cmd"
-	"goOrigin/pkg/utils"
-	"os"
 )
 
 func main() {
-	switch os.Getenv("mode") {
-	case "debug":
-		utils.NoError(cmd.RootCmd.Execute())
-	default:
-		cmd.PreRun("")
-		cmd.DebugServer()
-	}
+	//switch os.Getenv("mode") {
+	//case "debug":
+	fmt.Println(cmd.RootCmd.Execute())
+	//default:
+	//	cmd.PreRun("")
+	//	cmd.DebugServer()
+	//}
 
 }
