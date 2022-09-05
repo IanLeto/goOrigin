5f8225360f4794679b626ec9a3e55769bf829f51
ian.liu <ian.liu@ucloud.cn>
2022-09-05 15:25:02 +0800

    addlogs

--- pkg/logger/initlog.go
+++ pkg/logger/initlog.go
@@ -2,20 +2,32 @@ package logger
 
 import (
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
 )
 
-//var Logger *zap.SugaredLogger
 var Logger *zap.Logger
 
 func InitLogger() {
-	Logger, _ = zap.NewProduction()
-	//Logger = logger.Sugar()
-	//var (
-	//	encoder = zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
-	//	writeSyncer =
-	//)
-	//
-	//logger, _ = zap.New()
+	var (
+		encoder        zapcore.Encoder
+		writer         zapcore.WriteSyncer
+		core           zapcore.Core
+		consoleEncoder zapcore.Encoder
+	)
+	loggerConfig := zap.NewProductionEncoderConfig()
+	loggerConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoder = zapcore.NewJSONEncoder(loggerConfig)
+	consoleEncoder = zapcore.NewConsoleEncoder(loggerConfig)
+
+	file, _ := os.OpenFile("log.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	writer = zapcore.AddSync(file)
+
+	core = zapcore.NewTee(zapcore.NewCore(encoder, writer, zapcore.DebugLevel),
+		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), zapcore.DebugLevel))
+
+	Logger = zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+	return
 }
 
 func init() {
