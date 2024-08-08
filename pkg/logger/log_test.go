93ac2c4e62147131346a0f9f8aacd2a327893d20
ian <ccssliu@163.com>
2024-08-08 11:24:23 +0800

    x

--- pkg/logger/log_test.go
+++ pkg/logger/log_test.go
@@ -24,6 +24,24 @@ func (s *LoggerSuite) TestConfig() {
 	suger.Info("基础用法")
 }
 
+// TestMarshal :
+func (s *LoggerSuite) TestConfig2() {
+	var (
+		err error
+	)
+	conf := zap.Config{
+		Level:            zap.NewAtomicLevelAt(zap.ErrorLevel), // Set to ErrorLevel
+		Encoding:         "json",                               // 必填项
+		EncoderConfig:    zap.NewDevelopmentEncoderConfig(),    // 必填项
+		OutputPaths:      []string{"stdout"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+	s.log, err = conf.Build()
+	s.NoError(err)
+	s.log.Sugar().Info("不会打印")
+	s.log.Sugar().Error("会打印")
+}
+
 // TestHttpClient :
 func TestLogConfiguration(t *testing.T) {
 	suite.Run(t, new(LoggerSuite))
