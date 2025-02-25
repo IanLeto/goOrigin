86ef83ea8ea9c2cfc6f90663994ea852f4a712e0
ian <ccssliu@163.com>
2025-02-25 16:30:40 +0800

    x

--- API/V1/record.go
+++ API/V1/record.go
@@ -32,30 +32,6 @@ type QueryRequest struct {
 	Name string `json:"name"`
 }
 
-type QueryResponse struct {
-	Name       string `json:"name"`
-	CreateTime string `json:"create_time"`
-	UpdateTime string `json:"update_time"`
-	Body       struct {
-		Weight float32 `json:"weight"`
-	} `json:"body"`
-	BETre struct {
-		Core       int `json:"core"`
-		Runner     int `json:"runner"`
-		Support    int `json:"support"`
-		Squat      int `json:"squat"`
-		EasyBurpee int `json:"easy_burpee"`
-		Chair      int `json:"chair"`
-		Stretch    int `json:"stretch"`
-	} `json:"BETre"`
-	Worker struct {
-		Vol1 string `json:"vol1"`
-		Vol2 string `json:"vol2"`
-		Vol3 string `json:"vol3"`
-		Vol4 string `json:"vol4"`
-	} `json:"worker"`
-}
-
 type AppendRequestInfo struct {
 	Id   string `json:"id"`
 	Name string `json:"name"`
--- internal/router/recordHandlers/ian.go
+++ internal/router/recordHandlers/ian.go
@@ -8,31 +8,6 @@ import (
 	"goOrigin/internal/model/entity"
 )
 
-// param $参数名 参数类型 参数数据类型 是否必须 描述 其他
-
-// CreateIanRecord @Summary
-// @Description
-// @Tags RecordEntity
-// @Accept json
-// @param record body params.CreateIanRequestInfo true "1"
-// @Router /v1/record [POST]
-func CreateIanRecord(c *gin.Context) {
-	var (
-		req   = V1.CreateIanRequestInfo{}
-		objID interface{}
-		err   error
-	)
-	if err = c.ShouldBindJSON(&req); err != nil {
-		logrus.Errorf("%s", err)
-		goto ERR
-	}
-	//objID, err = logic.CreateIanRecord(c, req)
-	V1.BuildResponse(c, V1.BuildInfo(objID))
-	return
-ERR:
-	V1.BuildErrResponse(c, V1.BuildErrInfo(0, fmt.Sprintf("create recoed failed by %s", err)))
-}
-
 func DeleteIanRecord(c *gin.Context) {
 	var (
 		//id  = c.GetString("id")
@@ -40,26 +15,6 @@ func DeleteIanRecord(c *gin.Context) {
 		err error
 	)
 	//res, err = logic.DeleteIanRecord(c, id)
-	if err != nil {
-		goto ERR
-	}
-
-	V1.BuildResponse(c, V1.BuildInfo(res))
-	return
-ERR:
-	V1.BuildErrResponse(c, V1.BuildErrInfo(0, fmt.Sprintf("create recoed failed by %s", err)))
-}
-
-func UpdateIanRecord(c *gin.Context) {
-	var (
-		req = V1.CreateIanRequestInfo{}
-		res interface{}
-		err error
-	)
-	if err = c.ShouldBindJSON(&req); err != nil {
-		logrus.Errorf("%s", err)
-		goto ERR
-	}
 	if err != nil {
 		goto ERR
 	}
