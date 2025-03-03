57e922d1c81d18fe6016be68283fa0ed2f4dbc20
ian <ccssliu@163.com>
2025-03-03 18:11:26 +0800

    x

--- cmd/sync.go
+++ cmd/sync.go
@@ -1,1 +1,20 @@
 package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var syncCmd = &cobra.Command{
+	Use:   "sync",
+	Short: "Initialize resources like MySQL and Elasticsearch",
+	Run: func(cmd *cobra.Command, args []string) {
+		// 获取命令行参数
+
+	},
+}
+
+func init() {
+	// 添加命令行参数
+	syncCmd.Flags().StringArray("type", []string{"record", "github"}, "")
+
+}
